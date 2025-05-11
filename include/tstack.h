055004431c6981b8f7632316cef85428c41878de
Sibilka <barsik05072005@gmail.com>
2025-05-11 17:09:07 +0300

    tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,10 +1,24 @@
-// Copyright 2021 NNTU-CS
+// Copyright 2025 NNTU-CS
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
-
-template<typename T, int size>
+template <typename T, int SIZE>
 class TStack {
-  // добавьте код стека
-};
+ private:
+  T data[SIZE];
+  int topIndex;
 
+ public:
+  TStack() : topIndex(-1) {}
+  bool isEmpty() const { return topIndex == -1; }
+  bool isFull() const { return topIndex == SIZE - 1; }
+  void push(const T& value) {
+    if (!isFull()) data[++topIndex] = value;
+  }
+  T pop() {
+    return isEmpty() ? T() : data[topIndex--];
+  }
+  T peek() const {
+    return isEmpty() ? T() : data[topIndex];
+  }
+};
 #endif  // INCLUDE_TSTACK_H_
